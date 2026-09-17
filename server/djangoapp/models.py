e72b6c8771f3d771a1dfb2b41988ab9a344af496
mgwilliams112-ui <mgwilliams112@gmail.com>
2026-09-17 13:29:11 -0400

    Clean up models.py by removing unused code
    Removed unused imports and comments from models.py.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=50)
@@ -34,4 +30,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
