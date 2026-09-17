d9eb3ab975754ea6cd0b1f3ec4bbe75f7cebf371
mgwilliams112-ui <mgwilliams112@gmail.com>
2026-09-17 14:13:05 -0400

    Refactor CarModel class formatting
    Adjusted formatting and added whitespace for consistency.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     country_origin = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     CAR_TYPES = [
         ('CONVERTIBLE', 'Convertible'),
@@ -23,10 +26,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=11, choices=CAR_TYPES, default='SEDAN')
     year = models.IntegerField(default=2026,
-        validators=[
-            MaxValueValidator(2026),
-            MinValueValidator(2010)
-        ])
+           validators=[
+             MaxValueValidator(2026),
+             MinValueValidator(2010)
+           ])
 
     def __str__(self):
         return self.name
