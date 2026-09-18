9cce5d979e3e6ccff331c0e64ed63802906c9df1
mgwilliams112-ui <mgwilliams112@gmail.com>
2026-09-18 09:04:13 -0400

    Fix indentation of error logging in post_review

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -137,7 +137,7 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-	        print(f"Error: {e}")
+            print(f"Error: {e}")
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
