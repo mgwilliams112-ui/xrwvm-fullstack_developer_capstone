439cf7c9af0c405cfb911ad44506eb69af864261
mgwilliams112-ui <mgwilliams112@gmail.com>
2026-09-18 08:57:00 -0400

    Fix formatting of ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://mgwilliams11-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # pylint: disable=line-too-long
-CSRF_TRUSTED_ORIGINS = ['https://mgwilliams11-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # pylint: disable=line-too-long
+ALLOWED_HOSTS = ['localhost', 'https://mgwilliams11-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']  # pylint: disable=line-too-long
+CSRF_TRUSTED_ORIGINS = ['https://mgwilliams11-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']  # pylint: disable=line-too-long
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -94,7 +94,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # pylint: disable=line-too-long
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # pylint: disable=line-too-long
     },
     {
         'NAME':
